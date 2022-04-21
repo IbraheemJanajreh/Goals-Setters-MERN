e6e651f749214c1af9907c4b5be7b0b44ac1b605
Ibraheem <rf.nadal.22@gmail.com>
2022-04-22 00:49:34 +0300

    Goals Setter MERN

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const { errorHandler } = require("./middleware/error");
+const dotenv = require("dotenv").config();
+const port = process.env.PORT || 5000;
+const app = express();
+const connectDB = require("./config/db");
+connectDB();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use("/api/goals", require("./routes/goalRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
+app.use(errorHandler);
+app.listen(port, () => console.log(`Server started on port ${port}`));
